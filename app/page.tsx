8dc8a1350f060ed88b9bce1cc54b3bb01d6b8fc0
DiogoPLobo <92478902+DiogoPLobo@users.noreply.github.com>
2024-06-03 15:14:19 -0300

    palavra errada

--- app/page.tsx
+++ app/page.tsx
@@ -308,7 +308,7 @@ export default function Home() {
           </div>
         </div>
 
-        <p className="indent-14">Agora sabemos que é possível passar funções dentro de vetores. Agroa vamos tentar imaginar que essa função de transformação trata-se de outro vetor. A dinâmica se altera levemente, mas continua usando processor matemáticos básicos.
+        <p className="indent-14">Agora sabemos que é possível passar funções dentro de vetores. Vamos tentar imaginar que essa função de transformação trata-se de outro vetor. A dinâmica se altera levemente, mas continua usando processor matemáticos básicos.
          Quando falamos que há a convolução de 2 vetores, estamos fazendo uma opreação matemática entre dois vetores que irá resultar em um terceiro vetor.
         É só imaginarmos esses 2 vetores deslizando um abaixo do outro. Os valores que tiverem correspondentes serão multiplicados e somados entre si para resultar no valor do vetor resultante.:</p>
         <div className="flex flex-col w-full justify-center items-center">
